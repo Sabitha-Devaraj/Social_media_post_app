243ae779743282c14e1d14b72ebea7ac6a200c26
Sabitha-Devaraj <vdsabitha@gmail.com>
2023-12-29 00:23:37 +0000

    Learned the different way of routing the componenets

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import About from './About';
 import Missing from './Missing';
 import Footer from './Footer';
 import { Link, Route, Routes } from 'react-router-dom';
+import Post from './Post';
+import PostLayout from './PostLayout';
+
 
 function App() {
   
@@ -16,14 +19,27 @@ function App() {
       <nav>
         <ul>
           <li><Link to='/'>Home</Link></li>
+          <li><Link to='/header'>Header</Link></li>
+          <li><Link to='/nav'>Nav</Link></li>
           <li><Link to='/about'>About</Link></li>
-          <li><Link to='/newpost'>NewPost</Link></li>
+          <li><Link to='/postpage'>PostPage</Link></li>
+          <li><Link to='/footer'>Footer</Link></li>
         </ul>
       </nav>
       <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
-          <Route path='/newpost' element={<NewPost/>} />
+          <Route path='/header' element={<Header/>} />
+          <Route path='/nav' element={<Nav/>} />
+          <Route path='/footer' element ={<Footer/>}/>
+
+          <Route path='/postpage' element={<PostLayout/>} >
+            <Route index element={<PostPage/>} />
+            <Route path=':id' element={<Post/>} />
+            <Route path='newpost' element={<NewPost/>} />
+          </Route>
+          <Route path='*' element={<Missing/>} />
+
       </Routes>
     </div>
   );
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+//import { Link } from 'react-router-dom'
 
 const PostPage = () => {
   return (
     <main>
-            <h1>PostPage</h1>
+      <h1>PostPage</h1> 
+      
     </main>
   )
 }
